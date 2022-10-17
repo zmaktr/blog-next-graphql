c01ea364d34cf935a1d7e6180b933060d95b121c
unknown <zmaktr@gmail.com>
2022-10-17 13:37:50 +0500

    created components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import { PostCard, Categories, PostWidget } from "../components";
 
 const posts = [
   { title: "React Testing", excerpt: "Learn React Testing" },
@@ -9,18 +9,23 @@ const posts = [
 
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div className="container mx-auto px-10 mb-8 bg-gray-500">
       <Head>
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        {posts.map((post, index) => (
-          <div>
-            {post.title}
-            {post.excerpt}
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) => (
+            <PostCard post={post} key={post.title} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1 bg-green-600 border-2">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
